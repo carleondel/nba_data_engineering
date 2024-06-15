6f3424792566e94c67378d9d981f4bf877a478cb
Carlos León <carleondel@gmail.com>
2024-06-15 12:23:30 +0000

    folders structure created & base models yml created

--- models/staging/fivetran_nba_games_data/base/base_fivetran_nba_games_data__games_details_.sql
+++ models/staging/fivetran_nba_games_data/base/base_fivetran_nba_games_data__games_details_.sql
@@ -9,7 +9,8 @@ src_games_details as (
 renamed as (
 
     select
-        _fivetran_id,
+         -- Generating surrogate key
+        {{ dbt_utils.generate_surrogate_key(['TEAM_ID', 'PLAYER_ID', 'GAME_ID']) }} AS surrogate_key,
         team_id,
         ftm,
         fgm,
@@ -40,10 +41,15 @@ renamed as (
         fg3_pct,
         ft_pct,
         _fivetran_deleted,
-        _fivetran_synced
+        _fivetran_synced,
+
+        ROW_NUMBER() OVER (PARTITION BY surrogate_key ORDER BY surrogate_key) AS row_num
 
     from src_games_details
 
 )
 
-select * from renamed
+select * 
+from renamed
+where row_num = 1   -- Nos quedamos solo con la primera fila de cada surrogate_key
+-- (había 7 duplicados que eran errores de inserción en la BBDD)
