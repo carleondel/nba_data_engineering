f4fa673e24d9b7c1dc593befadb278ef2a4b2366
Carlos León <carleondel@gmail.com>
2024-06-17 16:40:31 +0000

    game_date added to games details from games, dim_date added to marts

--- models/staging/fivetran_nba_games_data/stg_fivetran_nba_games_data__games_details.sql
+++ models/staging/fivetran_nba_games_data/stg_fivetran_nba_games_data__games_details.sql
@@ -1,16 +1,19 @@
 -- Aquí estamos usando los dos modelos de base para filtrar games_details
--- y solo quedarnos con datos de season <= 2019
+-- y solo quedarnos con datos de season ENTRE 2009 Y 2019
+-- TAMBIEN COGEMOS LA FECHA
 WITH base_games AS (
     SELECT
-        game_id
+        game_id,
+        GAME_DATE_EST AS GAME_DATE
     FROM {{ ref('base_fivetran_nba_games_data__games_') }}
     -- Ya los he filtrado en base__games
-    --WHERE season <= 2019 
+    --WHERE season BETWEEN 2009 AND 2019 
 ),
 
 filtered_games_details AS (
     SELECT
-        gd.*
+        gd.*,
+        bg.GAME_DATE
     FROM {{ ref('base_fivetran_nba_games_data__games_details_') }} gd
     INNER JOIN base_games bg
     ON gd.game_id = bg.game_id
